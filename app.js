89ee29cbce5857a1ed474d72b965c85a1dc1876c
ali <ali@iMac-de-Elodie.fritz.box>
2020-08-17 20:53:36 +0200

    features ejs

--- app.js
+++ app.js
@@ -1,17 +1,87 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blogs')
 
 const app = express()
 
-app.listen(3000)
+const dbURL = 'mongodb+srv://ali:alibaba@cluster0-ekvmj.mongodb.net/node-blog?retryWrites=true&w=majority'
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+// app.get('/add', (req, res) => {
+//     const blog = new Blog({
+//         title: 'Yeni Yazi 2',
+//         short: 'kisa Aciklama',
+//         long: 'uzun aciklama'
+//     })
+
+//     blog.save()
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => console.log(err))
+// })
+
+// app.get('/all', (req, res) => {
+//     Blog.find()
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => console.log(err))
+// })
+
+// app.get('/single', (req, res) => {
+//     Blog.findById('5f3a5ebe77cf700793ca9967')
+//         .then((result) => {
+//             res.send(result)
+//         })
+//         .catch((err) => console.log(err))
+// })
+
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'Anasayfa', blogs: result })
+        })
+        .catch((err) => console.log(err))
+})
+
+app.get('/blog/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then((result) => {
+            res.render('blog', { title: 'Detay', blog: result })
+        })
+        .catch((err) => {
+            res.status(404).render('404', {
+                title: 'Sayfa Bulunamadi '
+            })
+        })
+
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'Hakkimizda' })
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Giris' })
 })
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+    res.status(404).render('404', {
+        title: 'Sayfa Bulunamadi '
+    })
+})  
